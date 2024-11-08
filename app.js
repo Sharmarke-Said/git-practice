5d2728b003494568abb2e3ba66b599106c63f201
Sharmarke-Said <sharmakesaid17@gmail.com>
2024-11-08 16:19:18 +0300

    Final changes

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-
-// Enable CORS
-app.use(cors());
 
 // Parse JSON bodies
 app.use(express.json());
 
 // Use routes
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
